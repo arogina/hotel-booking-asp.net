318e23ef170911ac8171c549fdef48bb41b17721
arogina <rogina.alan@gmail.com>
2022-05-14 15:19:54 +0200

    Initial comit

---
+++ Software/HotelBookingApp/HotelBookingMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelBookingMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

981e1a5f097c5485e1c9f1e2841ce977f44521fc
arogina <rogina.alan@gmail.com>
2022-08-03 17:44:23 +0200

    Added Security class

---
+++ Software/HotelBookingApp/BLL/Security.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+
+namespace BLL
+{
+    public class Security
+    {
+        public static string CreateSalt ()
+        {
+            byte[] salt = new byte[16];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetNonZeroBytes(salt);
+            }
+
+            return Convert.ToBase64String(salt);
+        }
+
+        public static string HashPassword (string password, string salt)
+        {
+            var saltBytes = Convert.FromBase64String(salt);
+
+            using (var rfc = new Rfc2898DeriveBytes(password, saltBytes, 1000))
+            {
+                return Convert.ToBase64String(rfc.GetBytes(16));
+            }
+        }
+    }
+}
